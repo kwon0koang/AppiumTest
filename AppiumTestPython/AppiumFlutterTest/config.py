cd305dc1bcb172d929ea06d71511c83915b5c2f4
kwon0koang <kwon0koang@gmail.com>
2023-11-06 23:49:08 +0900

    [kyk] iOS 테스트 중

--- AppiumTestPython/AppiumFlutterTest/config.py
+++ AppiumTestPython/AppiumFlutterTest/config.py
@@ -1,11 +1,55 @@
 appium_server_url = 'http://localhost:4723/wd/hub'
 
+# AOS
 capabilities = {
   "platformName": "Android",
-  "appium:deviceName": "R3CMA0F73PL",
+  "appium:deviceName": "Galaxy",
   "appium:appPackage": "com.example.test_flutter",
   "appium:appActivity": ".MainActivity",
   "appium:automationName": "UIAutomator2",
-  "appium:autoGrantPermissions": "true", # 권한 자동 허용
-  "appium:noReset": "true" # 앱 재설치 X
+  "appium:autoGrantPermissions": "true",
+  "appium:noReset": "true"
 }
+
+# todo kyk iOS
+# capabilities = {
+#   "platformName": "iOS",
+#   "appium:platformVersion": "15.4",
+#   "appium:deviceName": "iPhone SE",
+#   "appium:udid": "00008110-00167D812280401E",
+#   "appium:bundleId": "com.kykdev.testFlutter",
+#   "xcodeOrgId": "kwon0koang@naver.com",
+#   "xcodeSigningId": "Apple Developer",
+#   "appium:automationName": "XCUITest",
+#   "appium:autoGrantPermissions": "true",
+#   "appium:noReset": "true",
+#   "appium:allowProvisioningDeviceRegistration": "true",
+#   "usePrebuildWDA": "true"
+# }
+
+# Temp
+# "appium:udid": "auto",
+# "appium:app": "com.kykdev.testFlutter",
+# "appium:appName": "com.kykdev.testFlutter",
+
+# iOS Success (https://velog.io/@tkjung/Appium-iOS-%ED%99%98%EA%B2%BD-%EC%85%8B%ED%8C%85#xcode%EC%97%90%EC%84%9C-webdriveragent-signing--build-settings-%EC%84%A4%EC%A0%95)
+# 실물 기기
+# {
+#   "platformName": "ios",
+#   "appium:deviceName": "iPhoneX",
+#   "appium:platformVersion": "15.2.1", 
+#   "appium:automationName": "xcuitest",
+#   "appium:udid": "{UDID}",
+#   "appium:bundleId": "{실행할 앱의 Bundle ID}",
+#   "appium:xcodeOrgId": "{애플 개발자 계정에서 식별해둔 Team ID}",
+#   "appium:xcodeSigningId": "iPhone Developer"
+# }
+# 시뮬레이터
+# {
+#   "platformName": "ios",
+#   "appium:automationName": "xcuitest",
+#   "appium:deviceName": "simulator",
+#   "appium:platformVersion": "14.1",
+#   "appium:udid": "{UDID}",
+#   "appium:bundleId": "{실행할 앱의 Bundle ID}"
+# }
