8a7f0513c6bde03acdefa337c3a82c7697be8d87
kwon0koang <kwon0koang@gmail.com>
2023-11-27 23:12:07 +0900

    [kyk] test selenium & appium base

--- AppiumTestPython/appium_flutter_test/test_appium.py
+++ AppiumTestPython/appium_flutter_test/test_appium.py
@@ -1,12 +1,32 @@
 from appium import webdriver
-import config
-import util
 from appium.webdriver.common.appiumby import AppiumBy
 import time
+from appium.options.android import UiAutomator2Options
 
-driver = webdriver.Remote(command_executor=config.appium_server_url(), options=util.get_capabilities_options())
+# 테스트 앱 열기
+capabilities = UiAutomator2Options().load_capabilities({
+  "platformName": "android",
+  "appium:deviceName": "R3CMA0F73PL",
+  "appium:automationName": "uiautomator2",
+  "appium:appPackage": "com.example.test_flutter",
+  "appium:appActivity": ".MainActivity",
+  "appium:autoGrantPermissions": "true",
+  "appium:noReset": "true"
+})
+driver = webdriver.Remote(command_executor="http://localhost:4723", options=capabilities)
 
+time.sleep(3)
+
+# "Todo list" 클릭
 el = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Todo list")
 el.click()
 
-time.sleep(5)
+time.sleep(3)
+
+
+
+
+
+
+
+
--- AppiumTestPython/appium_flutter_test/test_selenium.py
+++ AppiumTestPython/appium_flutter_test/test_selenium.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 
-# 네이버 키기
+# 네이버 열기
 driver.get("https://www.naver.com/")
+
+time.sleep(3)
 
 # "경제" 찾아보기
 el = driver.find_element(By.LINK_TEXT, "경제")
@@ -16,10 +19,15 @@ el.get_attribute("class")
 # "경제" 클릭
 el.click()
 
+time.sleep(3)
+
 # 애플 검색하기
 el = driver.find_element(By.CLASS_NAME, "search_input")
 el.send_keys("AAPL")
-el = driver.find_element(By.CLASS_NAME, "btn_search")
-el.click()
+el.send_keys(Keys.ENTER)
 
-time.sleep(5)
+time.sleep(3)
+
+
+
+
