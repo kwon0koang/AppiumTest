7f8020b883a37b2ab3b63081e827ecd6a44a0a4f
kwon0koang <kwon0koang@gmail.com>
2024-01-11 08:09:40 +0900

    [kyk] config 정리

--- AppiumTestPython/appium_flutter_test/config.py
+++ AppiumTestPython/appium_flutter_test/config.py
@@ -54,17 +54,6 @@ capabilities = {
     "appium:autoGrantPermissions": "true",
     "appium:noReset": "true"
   },
-  "aos_galaxy_s_22_ultra" : {
-    "platformName": "Android",
-    "appium:deviceName": "aos_galaxy_s_22_ultra",
-    "appium:udid": "R3CT10E16RK",
-    "appium:systemPort": "8204",
-    "appium:automationName": "UiAutomator2",
-    "appium:appPackage": "com.example.test_flutter",
-    "appium:appActivity": ".MainActivity",
-    "appium:autoGrantPermissions": "true",
-    "appium:noReset": "true"
-  },
   "ios_iphone_se_2022" : {
     "platformName": "iOS",
     "appium:deviceName": "ios_iphone_se_2022",
