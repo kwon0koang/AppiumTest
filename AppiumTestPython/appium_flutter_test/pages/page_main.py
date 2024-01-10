09ee1dfec60d4b4860145f9bbbfee19f532d0c3b
kwon0koang <kwon0koang@gmail.com>
2024-01-10 21:22:01 +0900

    [kyk] ID 수정

--- AppiumTestPython/appium_flutter_test/pages/page_main.py
+++ AppiumTestPython/appium_flutter_test/pages/page_main.py
@@ -7,6 +7,6 @@ class MainPage(BasePage):
 
     def click_todo_list(self):
         id_todo_list = util.get_value(aos="Todo list", ios="Todo list")
-        util.click(self.driver, AppiumBy.ID, id_todo_list)
+        util.click(self.driver, AppiumBy.ACCESSIBILITY_ID, id_todo_list)
 
 
--- AppiumTestPython/appium_flutter_test/pages/page_todo_list.py
+++ AppiumTestPython/appium_flutter_test/pages/page_todo_list.py
@@ -17,13 +17,13 @@ class TodoListPage(BasePage):
         
     def check_todo_item(self):
         id_todo_item = util.get_value(aos="2 / 27 / true / veritatis pariatur delectus", ios="2 / 27 / true / veritatis pariatur delectus")
-        el = util.wait(self.driver, AppiumBy.ID, id_todo_item)
+        el = util.wait(self.driver, AppiumBy.ACCESSIBILITY_ID, id_todo_item)
         content_desc = el.get_attribute("content-desc")
         self.testCase.assertEqual(content_desc, '2 / 27 / true / veritatis pariatur delectus', '검증 실패 :(')
 
     def click_todo_item(self):
         id_todo_item = util.get_value(aos="2 / 27 / true / veritatis pariatur delectus", ios="2 / 27 / true / veritatis pariatur delectus")
-        util.click(self.driver, AppiumBy.ID, id_todo_item)
+        util.click(self.driver, AppiumBy.ACCESSIBILITY_ID, id_todo_item)
 
     def scroll_up(self):
         util.scroll(self.driver, util.ScrollDirection.UP)
